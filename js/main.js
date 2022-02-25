da0300ef8f81547684d8f2002ee0f4a03fff173e
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-02-25 11:41:14 +0100

    Add Skip Button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
+$("#skip").click(function () {
+    $("span").addClass("brume")
+    if ($("span.brume").length === $("span").length) {
+        // Si tout les spans ont disparu alors:
+        $("body").css('animation', 'zoom forwards 1.25s linear');
+        $("body").css('overflow', 'hidden');
+        $("#firstProject").css("display", "block");
+        $("#firstProject").css("animation", "blurIn 2s linear");
+        $("#firstProject").css("animation-fill-mode", "forwards");
+        $("#firstProject").css("cursor", "text");
+    }
+})
+
 $("span").mouseenter(function () {
     $(this).addClass("brume");
     if ($("span.brume").length === $("span").length) {
-    // Si tout les spans ont disparu alors:
+        // Si tout les spans ont disparu alors:
         $("body").css('animation', 'zoom forwards 1.25s linear');
         $("body").css('overflow', 'hidden');
         $("#firstProject").css("display", "block");
