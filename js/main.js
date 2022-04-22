b1566750265265aee4979bfa80592416dd20223d
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-04-22 17:36:00 +0200

    Finish Button

--- js/main.js
+++ js/main.js
@@ -34,7 +34,6 @@ $("#skip").click(function () {
     }
 })
 
-
 $("span").mouseenter(function () {
     if ($("span.brume").length !== $("span").length) {
         $(this).addClass("brume");
@@ -91,8 +90,74 @@ $("#firstLeftButton").click(function () {
     $("#textFirstProject").css("display", "block");
     $("#textFirstProject").css("animation", "blurOut 1s linear");
     $("#textFirstProject").css("animation-fill-mode", "forwards");
+    $("#firstLeftButton").css("animation", "blurOut 2s linear")
+    $("#firstLeftButton").css("display", "none");
+    $("#firstLeftButton").css("animation-fill-mode", "forwards");
+    $("#firstRightButton").css("animation", "blurOut 2s linear")
+    $("#firstRightButton").css("display", "none");
+    $("#firstRightButton").css("animation-fill-mode", "forwards");
     $("header").css("background-image", "url('/img/profile.png')")
     $("header").css("animation-fill-mode", "forwards")
+    setTimeout(() => {
+        $("#monProfil").css("display", "block");
+        $("#monProfil").css("animation", "blurIn 2s linear");
+        $("#monProfil").css("animation-fill-mode", "forwards");
+        $("#monProfil").css("cursor", "text");
+        $("#profilLeftButton").css("animation", "blurIn 2s linear")
+        $("#profilLeftButton").css("display", "block");
+        $("#profilLeftButton").css("animation-fill-mode", "forwards");
+        $("#profilRightButton").css("animation", "blurIn 1.5s linear")
+        $("#profilRightButton").css("display", "block");
+        $("#profilRightButton").css("animation-fill-mode", "forwards");
+        $("#profilRightText").css("display", "block");
+        $("#profilRightText").css("animation", "blurIn 2s linear");
+        $("#profilRightText").css("animation-fill-mode", "forwards");
+    }, 500)
+})
+
+$("#profilRightButton").click(function () {
+    $("#monProfil").css("display", "block");
+    $("#monProfil").css("animation", "blurOut 2s linear");
+    $("#monProfil").css("animation-fill-mode", "forwards");
+    $("#monProfil").css("cursor", "text");
+    $("#profilLeftButton").css("animation", "blurOut 2s linear")
+    $("#profilLeftButton").css("display", "none");
+    $("#profilLeftButton").css("animation-fill-mode", "forwards");
+    $("#profilRightButton").css("animation", "blurOut 1.5s linear")
+    $("#profilRightButton").css("display", "none");
+    $("#profilRightButton").css("animation-fill-mode", "forwards");
+    $("#profilRightText").css("animation", "blurOut 1.5s linear");
+    $("#profilRightText").css("display", "none");
+    $("#profilRightText").css("animation-fill-mode", "forwards");
+    $("header").css("background-image", "url('/img/header.jpg')")
+    $("header").css("animation-fill-mode", "forwards")
+    setTimeout(() => {
+        $("#firstProject").css("display", "block");
+        $("#firstProject").css("animation", "blurIn 1s linear");
+        $("#firstProject").css("animation-fill-mode", "forwards");
+        $("#firstProject").css("cursor", "text");
+        $("#firstLeftButton").css("animation", "blurIn 1s linear")
+        $("#firstLeftButton").css("display", "block");
+        $("#firstLeftButton").css("animation-fill-mode", "forwards");
+        $("#firstRightButton").css("animation", "blurIn 1s linear")
+        $("#firstRightButton").css("display", "block");
+        $("#firstRightButton").css("animation-fill-mode", "forwards");
+        $("#pictureFirstProject").css("display", "block");
+        $("#pictureFirstProject").css("animation", "blurIn 1.5s linear");
+        $("#pictureFirstProject").css("animation-fill-mode", "forwards");
+        $("#linkFirstProject").css("display", "block");
+        $("#linkFirstProject").css("animation", "blurIn 1.5s linear");
+        $("#linkFirstProject").css("animation-fill-mode", "forwards");
+        $("#firstLeftText").css("display", "block");
+        $("#firstLeftText").css("animation", "blurIn 2s linear");
+        $("#firstLeftText").css("animation-fill-mode", "forwards");
+        $("#firstRightText").css("display", "block");
+        $("#firstRightText").css("animation", "blurIn 2s linear");
+        $("#firstRightText").css("animation-fill-mode", "forwards");
+        $("#textFirstProject").css("display", "block");
+        $("#textFirstProject").css("animation", "blurIn 1.5s linear");
+        $("#textFirstProject").css("animation-fill-mode", "forwards");
+    }, 1250)
 })
 
 $("#firstRightButton").click(function () {
@@ -113,6 +178,41 @@ $("#firstRightButton").click(function () {
     $("#textFirstProject").css("display", "block");
     $("#textFirstProject").css("animation", "blurOut 1s linear");
     $("#textFirstProject").css("animation-fill-mode", "forwards");
+    $("#firstLeftButton").css("animation", "blurOut 2s linear")
+    $("#firstLeftButton").css("display", "block");
+    $("#firstLeftButton").css("animation-fill-mode", "forwards");
+    $("#firstRightButton").css("animation", "blurOut 2s linear")
+    $("#firstRightButton").css("display", "block");
+    $("#firstRightButton").css("animation-fill-mode", "forwards");
+    $("header").css("background-image", "url('/img/2ndProject.jpg')")
+    $("header").css("animation-fill-mode", "forwards")
+    setTimeout(() => {
+        $("#secondProject").css("display", "block");
+        $("#secondProject").css("animation", "blurIn 1s linear");
+        $("#secondProject").css("animation-fill-mode", "forwards");
+        $("#secondProject").css("cursor", "text");
+        $("#secondLeftButton").css("animation", "blurIn 1s linear")
+        $("#secondLeftButton").css("display", "block");
+        $("#secondLeftButton").css("animation-fill-mode", "forwards");
+        $("#secondRightButton").css("animation", "blurIn 1s linear")
+        $("#secondRightButton").css("display", "block");
+        $("#secondRightButton").css("animation-fill-mode", "forwards");
+        $("#pictureSecondProject").css("display", "block");
+        $("#pictureSecondProject").css("animation", "blurIn 1.5s linear");
+        $("#pictureSecondProject").css("animation-fill-mode", "forwards");
+        $("#linkSecondProject").css("display", "block");
+        $("#linkSecondProject").css("animation", "blurIn 1.5s linear");
+        $("#linkSecondProject").css("animation-fill-mode", "forwards");
+        $("#secondLeftText").css("display", "block");
+        $("#secondLeftText").css("animation", "blurIn 2s linear");
+        $("#secondLeftText").css("animation-fill-mode", "forwards");
+        $("#secondRightText").css("display", "block");
+        $("#secondRightText").css("animation", "blurIn 2s linear");
+        $("#secondRightText").css("animation-fill-mode", "forwards");
+        $("#textSecondProject").css("display", "block");
+        $("#textSecondProject").css("animation", "blurIn 1.5s linear");
+        $("#textSecondProject").css("animation-fill-mode", "forwards");
+    }, 1250)
 })
 
 
